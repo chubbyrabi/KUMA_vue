d7b297bba1c1a3887841d89e8a266783487bca94
ellen <112414227+chubbyrabi@users.noreply.github.com>
2025-03-15 15:52:34 +0800

    up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,12 @@ jobs:
         run: |
           git config --global user.name "chubbyrabi"
           git config --global user.email "chubbyrabi@gmail.com"
-
-      - name: Show git configuration
-        run: |
-          git config --list
+          git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}.git
 
       - name: Set up Node.js
         uses: actions/setup-node@v2
         with:
-          node-version: '20'
+          node-version: '20'  # 使用你的 Node.js 版本
 
       - name: Install dependencies
         run: npm install
@@ -33,12 +30,14 @@ jobs:
       - name: Build the app
         run: npm run build
 
-      - name: List files in dist
-        run: |
-          ls -al dist  # 查看 dist 目录中的文件
-
       - name: Deploy to GitHub Pages
         uses: JamesIves/github-pages-deploy-action@4.1.3
         with:
-          branch: gh-pages
-          folder: dist
+          branch: gh-pages  # 部署的目標分支
+          folder: dist      # 部署的文件夾，應該是構建結果的文件夾
+
+      - name: Force push to gh-pages
+        run: |
+          git add -A
+          git commit -m "Deploy to gh-pages"
+          git push --force origin gh-pages
